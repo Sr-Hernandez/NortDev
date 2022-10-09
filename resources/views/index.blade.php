acbe976c6063ba9e6bbed11c4a06c6a5415a233e
Berkeley10 <62038944+Berkeley10@users.noreply.github.com>
2022-10-08 18:14:54 -0600

    -v 1.0.0
    antes de agregar combobox en view contacto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
             <h1 class="subtitle">Pon tu negocio <strong>Online</Strong></h1>
             <h1 class="title" id="tittle"></h1>  
         </div>  
-        <div class="shape">
+        <div class="shape"> 
            
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
   <path fill="#171717" fill-opacity="1" d="M0,288L80,272C160,256,320,224,480,224C640,224,800,256,960,272C1120,288,1280,288,1360,288L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
