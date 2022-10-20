66fe283f5912c63118e32cf16822f6cf4e2840d8
Angel Hernandez <angel.hernandez@mdn.mx>
2022-10-20 17:58:16 -0600

    service table barber

--- resources/views/portafoliobarber.blade.php
+++ resources/views/portafoliobarber.blade.php
@@ -22,7 +22,7 @@
 					<!-- Logo -->
 						<div id="logo">
 							<h1><a >Tu Nombre Aqui</a></h1>
-							<p>BARBERSHOP - SALÓN DE BARBERÍA Y PELUQUERÍA </p>
+							<p>BARBERSHOP - SALÓN DE BARBERÍA Y PELUQUERÍA</p>
 							
 						</div>
 
@@ -71,8 +71,38 @@
 					<div class="container">
 						<p class="style1">So in case you were wondering what this is all about ...</p>
 						<p class="style2">
-							Escape Velocity is a free responsive<br class="mobile-hide" />
-							site template by <a href="http://html5up.net" class="nobr">HTML5 UP</a>
+							<style>
+								.card-b{
+
+								}
+							</style>
+							<table>
+								<tbody>
+									<tr>
+										<td>
+											<div class="card-b">
+												<div class ="card-img">
+
+													<img src="assets/imgs/barber-service-1.jpg" alt="" />
+												</div>
+												123
+											</div>
+										</td>
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+								</tbody>
+							</table>
 						</p>
 						<p class="style3">It's <strong>responsive</strong>, built on <strong>HTML5</strong> and <strong>CSS3</strong>, and released for
 						free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0 license</a>, so use it for any of
