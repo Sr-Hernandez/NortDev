3708f90a0d5c35a0819e7297902142477b06cba8
Angel Hernandez <angel.hernandez@mdn.mx>
2022-10-26 17:51:05 -0600

    CV avance

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -3,19 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/cv.css') }}">
-    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Play:wght@700&display=swap" rel="stylesheet">
     <title>CV-Ángel Hernández</title>
 
    
 </head>
 <header>
     <div class="header-cv">
-        <h1>
-            CV
-        </h1>
-        <div class="foto">
-            123
-        </div>
+
+        <table>
+            <tbody>
+                <tr>
+                    <td >
+                        <h1>
+                            {{-- Ángel Hernández Urquijo --}}
+                            <span>ÁNGEL</span>
+                            <span>HERNÁNDEZ</span><br>
+                            <span>URQUIJO</span>
+                        </h1>
+                        <h2>
+                            Ingeniero en Sistemas Computacionales
+                        </h2>
+                    </td>
+                    <td>
+                        <div class="foto">
+                            123
+                        </div>
+                    </td>
+                    <td>
+                        <p>Abaout</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+       
+       
     </div>
 </header>
 <body>
