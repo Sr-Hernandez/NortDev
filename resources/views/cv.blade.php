8d79c6807be8870941f21e60ecee25749ccbc222
Angel Hernandez <angel.hernandez@mdn.mx>
2022-11-04 17:12:47 -0700

    Cards y logos
    Se agregan 4 cards y los logos html, css, laravel, aws

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/cv.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/icofont.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Play:wght@700&display=swap" rel="stylesheet">
@@ -11,6 +12,8 @@
    
 </head>
 <header>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    
     <div class="header-cv">
 
         <table class="tabla-header">
@@ -35,7 +38,8 @@
                         </div>
                     </td>
                     <td>
-                        <p>Abaoutt</p>
+                        <h1>Abaoutt</h1>
+                        <p>text 213...</p>
                     </td>
                 </tr>
             </tbody>
@@ -44,16 +48,142 @@
        
     </div>
 </header>
-123
 
 <body>
+    <div class="loader"></div>
+    <script type="text/javascript">
+        $(window).load(function() {
+            $(".loader").fadeOut("slow");
+        });
+        </script>
     <div class="separator1"></div>
-    <p>123</p>
-    <table>
+    <div class="main-container">
+        <div class="heading">
+          <h1 class="heading__title">Tecnologias usadas aqui</h1>
+        </div>
+        <div class="cards">
+          <div class="card card-1">
+            {{-- <div class="card__icon"><i class="fas fa-bolt"></i></div>
+            <p class="card__exit"><i class="fas fa-times"></i></p> --}}
+            <img class="img-cards" src="assets/imgs/logo_html_sin_fondo.png" alt="">
+            {{-- <h2 class="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2> --}}
+            
+            <p class="card__apply">
+              {{-- <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a> --}}
+            </p>
+          </div>
+          <div class="card card-2">
+            {{-- <div class="card__icon"><i class="fas fa-bolt"></i></div>
+            <p class="card__exit"><i class="fas fa-times"></i></p> --}}
+            <img class="img-cards" src="assets/imgs/logo_css_sin_fondo.png" alt="">
+            {{-- <h2 class="card__title">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2> --}}
+            <p class="card__apply">
+              {{-- <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a> --}}
+            </p>
+          </div>
+          <div class="card card-3">
+            {{-- <div class="card__icon"><i class="fas fa-bolt"></i></div>
+            <p class="card__exit"><i class="fas fa-times"></i></p> --}}
+            <img class="img-cards" src="assets/imgs/logo_laravel_sin_fondo.png" alt="">
+            {{-- <h2 class="card__title">Ut enim ad minim veniam.</h2> --}}
+            <p class="card__apply">
+              {{-- <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a> --}}
+            </p>
+          </div>
+          <div class="card card-4">
+            {{-- <div class="card__icon"><i class="fas fa-bolt"></i></div>
+            <p class="card__exit"><i class="fas fa-times"></i></p> --}}
+            <img class="img-cards" src="assets/imgs/logo_aws_sin_fondo.png" alt="">
+            {{-- <h2 class="card__title">Quis nostrud exercitation ullamco laboris nisi.</h2> --}}
+            <p class="card__apply">
+              {{-- <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a> --}}
+            </p>
+          </div>
+     
+        </div>
+      </div>
+   <section class="section-1">
+    <div class="div-exp">
+        <table class="sticky-table-column">
+            <thead>
+                <tr>
+                    <th class="sticky-table-column__sticky">...</th>
+                        <p><i class="icofont icofont-verification-check"></i>text...</p>
+                        <p><i class="icofont-verification-check"></i>text...</p>
+                        <p><i class="icofont-verification-check"></i>text...</p>
+                    <!-- Other header column ... -->
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="sticky-table-column__sticky">...</td>
+                    <p>qwe</p>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+   </section>
+    <table class="tabla-body-1">
         <tr>
             <td>
-                <p>Tecnologiass</p>
-                <h4>123</h4>
+                <div class="card">
+                    <!-- Cover -->
+                    <div class="card__cover">
+                        <img class="img-card" src="https://angelhernandez.work/assets/imgs/logo_html.png" alt="">
+                    </div>
+                
+                    <!-- Content -->
+                    <div class="card__content">.123..</div>
+                    ...
+                </div>
+            </td>
+            <td>
+                <div class="card">
+                    <!-- Cover -->
+                    <div class="card__cover">
+                        <img class="img-card" src="https://angelhernandez.work/assets/imgs/logo_html.png" alt="">
+                    </div>
+                
+                    <!-- Content -->
+                    <div class="card__content">.123..</div>
+                    ...
+                </div>
+            </td>
+            <td>
+                <div class="card">
+                    <!-- Cover -->
+                    <div class="card__cover">
+                        <img class="img-card" src="https://angelhernandez.work/assets/imgs/logo_html.png" alt="">
+                    </div>
+                
+                    <!-- Content -->
+                    <div class="card__content">.123..</div>
+                    ...
+                </div>
+            </td>
+            <td>
+                <div class="card">
+                    <!-- Cover -->
+                    <div class="card__cover">
+                        <img class="img-card" src="https://angelhernandez.work/assets/imgs/logo_html.png" alt="">
+                    </div>
+                
+                    <!-- Content -->
+                    <div class="card__content">.123..</div>
+                    ...
+                </div>
+            </td>
+            <td>
+                <div class="card">
+                    <!-- Cover -->
+                    <div class="card__cover">
+                        <img class="img-card" src="https://angelhernandez.work/assets/imgs/logo_html.png" alt="">
+                    </div>
+                
+                    <!-- Content -->
+                    <div class="card__content">.123..</div>
+                    ...
+                </div>
             </td>
         </tr>
     </table>
