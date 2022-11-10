00026d12cfd8f48cf0d38e6d4fde56eeed0a4a3c
Angel Hernandez <angel.hernandez@mdn.mx>
2022-11-10 12:53:27 -0700

    footer beta 1

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -187,7 +187,7 @@ Photoshop. --}}
      
         </div>
       </div>
-   <section class="section-1 sticky-table-column">
+   {{-- <section class="section-1 sticky-table-column">
     <div class="div-exp">
         <table >
                 <tr class="">
@@ -270,6 +270,20 @@ Photoshop. --}}
                 </div>
             </td>
         </tr>
-    </table>
+    </table> --}}
 </body>
+
+<div class="footer-basic">
+    <footer>
+        <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Home</a></li>
+            <li class="list-inline-item"><a href="#">Services</a></li>
+            <li class="list-inline-item"><a href="#">About</a></li>
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        </ul>
+        <p class="copyright">Company Name © 2018</p>
+    </footer>
+</div>
 </html>
