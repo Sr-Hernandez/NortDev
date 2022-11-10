c123b26f16c0688592e6f2f630470520451df9aa
Berkeley10 <62038944+Berkeley10@users.noreply.github.com>
2022-11-10 13:10:06 -0600

    footer acomodado

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/icofont.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/icofont.min.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Play:wght@700&display=swap" rel="stylesheet">
     <title>CV-Ángel Hernández</title>
