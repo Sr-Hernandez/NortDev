3a726028f7f215b629ef841e269140a02b1bf846
Angel Hernandez <angelurquijo10@gmail.com>
2022-12-19 13:29:35 -0700

    Update alertas.js

--- public/assets/js/alertas.js
+++ public/assets/js/alertas.js
@@ -40,16 +40,7 @@ document.body.removeChild(aux);
   }
 
   function guardarmensaje(result){
-    $.ajax({
-      url: "contacto_mensjae",
-      method: "GET",
-      data: {
-        name: "CV",
-        email:"CV",
-        phone1 :"CV",
-        mensaje: result
-      }
-    });
+  
 
   }
 
